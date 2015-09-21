efdac36aeae1b129f38cb8c5218eecae34215d35
tacoen <tacoen@gmail.com>
2015-09-21 11:58:14 +0900

    Update htmlize.class.php

--- htmlize.class.php
+++ htmlize.class.php
@@ -1,75 +1,107 @@
 <?php
-
-/* example for call_user_func */
-
-function dummy_func() { return file_get_contents('dummy.txt'); }
-
 /*
  * htmlize
  *
  * (c) tacoen, dibuat untuk mendukung pemalasan diri.
  */
 
+/* -- User function -------------------------------------------------------------------------------------- */
+
+function test_func($w) { 
+	return "$w[0] and $w[1] are friendly element"; 
+}
+
+/* -- Class -------------------------------------------------------------------------------------- */
+ 
 class htmlize {
 
 	public function __construct() {
-		$this->q=''; //query
-	}
-
-	public function start($func=NULL,$array_passed=array()) { 
-		if ((isset($func)) && (function_exists($func."_func"))) { call_user_func($func."_func",$array_passed); } 
-		echo "<html>\n"; 
-	}
-
-	public function end($func=NULL,$array_passed=array()) { 
-		if ((isset($func)) && (function_exists($func."_func"))) { call_user_func($func."_func",$array_passed); } 
-		echo "</html>"; 
+		$this->_script=array(); 
+		$this->_style=array(); 
 	}
 	
-	public function create($w,$id=NULL,$attr=NULL) { 
-		if (isset($id)) { $this->q[$w]['id']=$id; }
-		if (isset($attr)) { $this->q[$w]['attr']=$attr; }
-		$this->q[$w]['html']='';
+	public function create_container($container,$tag="div",$attr=NULL) {
+		$this->{$container}['index']=0; 
+		$this->{$container}['tag']=$tag; 
+		$this->{$container}['attr']=$attr; 
+		$this->{$container}['cache']=NULL; 
 	}
 	
-	public function query($w,$func=NULL,$array_passed=array()) { 
-		if (isset($func)) { 
-			if ( function_exists($func."_func")) { $html=call_user_func($func."_func",$array_passed); } else { $html=$func; }
+	public function set_cache($container,$mode=false,$expire=0) {
+		if (!isset($this->{$container})) { throw new Exception(__METHOD__." '$container' not found"); }
+		$this->{$container}['file']="_".get_class($this)."_".$container; 
+		if ($mode==false) {
+			$this->{$container}['cache'] = 0; 
+		} else {
+			if ($expire==0) { htmlize_delCache($this->{$container}['file']); }
+			$this->{$container}['cache']=htmlize_valCache($this->{$container}['file'], $expire, $mode); 
 		}
-		$this->q[$w]['html'] .=$html; 
+	}
+
+	public function query($container,$element,$func=NULL,$attr=NULL,$order=NULL,$array_passed=array(),$enclose=false ) {
+		if (!isset($this->{$container})) { throw new Exception(__METHOD__." '$container' not found"); }
+		if ($this->{$container}['cache']==1 ) { return; }
+		$i=$this->{$container}['index']++; if (!$order) { $order=$i; } 
+		$this->{$container}['Q'][$i]['o']=$order; 
+		$html="<$element"; if($attr) { $html .=" $attr"; } 
+		if ($enclose==false) { $html .=">"; } else { $html .="/>"; }
+		if (function_exists($func."_func")) { $html .=call_user_func($func."_func",$array_passed); } else { $html .=$func; }
+		if ($enclose==false) { $html .="</$element>"; }
+		$this->{$container}['Q'][$i]['h']=$html; 
+		sort($this->{$container}['Q']); 
+	}
+
+	public function add($container,$func,$name=NULL,$array_passed=array()) {
+		if (!isset($this->{$container})) { throw new Exception(__METHOD__." '$container' not found"); }
+		if ($this->{$container}['cache']==1 ) { return; }
+		$i=$this->{$container}['index']++; 
+		if (function_exists($func."_func")) { $html =call_user_func($func."_func",$array_passed); } else { $html =$func; }
+		$this->{$container}['Q'][$i]['h']=$html; 
+		if ($name) { $this->{$container}['Q'][$i]['n']=$name; } 
 	}
 	
-	public function append($w,$s){ $this->q[$w]['html'] .=$s; }
+	public function build($container,$close=true) {
 
-	public function build($w,$func=NULL,$array_passed=array()) {
-		
-		if ( isset($this->q[$w])) { $el=$this->q[$w]; } else { $el=array(); }
-		
-		$html="<$w";
-		if (isset($el['id'])) { $html .=" id='".$el['id']."'"; } 
-		if (isset($el['attr'])) { $html .=" ".$el['attr']; }
-		$html .=">\n";
-		$html .=$el['html'];
-		if (isset($func)) { 
-			if ( function_exists($func."_func")) { $html .=call_user_func($func."_func",$array_passed); }
+		if (!isset($this->{$container})) { throw new Exception(__METHOD__." '$container' not found"); }
+		if ($this->{$container}['cache']==1 ) { 
+			$this->{$container}['action']="read"; 
+			return htmlize_getCache($this->{$container}['file']); 
 		}
-		$html .="</$w>\n";
-			
-		echo $html;
-	}
-	
-}
 
-class formize {
-	
-	public function __construct($action,$method) { 
-		$this->field=array();
-		$this->index=0;
-		$this->action=$action;
-		$this->method=$method;
+		$html=''; 
+		
+		if ($this->{$container}['tag'] ) { 
+			$html .="<".$this->{$container}['tag']; 
+			if ($this->{$container}['attr']) { $html .=" ".$this->{$container}['attr']; }
+			$html .=">".PHP_EOL; $has_tag=true; 
+		}
+		
+		foreach ($this->{$container}['Q'] as $q ) { $html .=$q['h'].PHP_EOL; }
+		
+		if ($has_tag) { $html .="</".$this->{$container}['tag'].">".PHP_EOL; }
+
+		if ($this->{$container}['cache']==2 ) { 
+			$this->{$container}['action']=htmlize_putCache($this->{$container}['file'],$html); 
+		}
+		
+		return $html; 
+	}
+
+/* -- more ------------------------------------------------------------------------------------------------- */
+
+	public function style_query($container ,$name, $url, $media, $order=NULL) {
+		$this->query($container,'link',NULL,"rel='stylesheet' type='text/css' media='$media' id='css_".htmlize_sfst($name)."' href='$url'",$order, NULL, true); 
+		$this->_style[] = $url; 
 	}
 	
-	public function input($name,$val,$type) {
+	public function script_query($container ,$name, $url ,$order=NULL) {
+		$this->query($container,'script',NULL,"type='text/javascript' id='js_".htmlize_sfst($name)."' src='$url'",$order); 
+		$this->_script[] = $url; // collect with preg match ^http://.+?\w+?/
+	}
+	
+/* -- forms ------------------------------------------------------------------------------------------------ */
+	
+	public function fq_input($name,$val,$type) {
 		switch($type) {
 			case 'submit':
 				$html='';
@@ -87,7 +119,7 @@ class formize {
 		return $html;
 	}
 	
-	public function select($name,$val,$option) {
+	public function fq_select($name,$val,$option) {
 		$html="<select name='$name'>";
 		foreach($option as $o) { 
 			if ($o==$val) { $s=" selected"; } else { $s=''; }
@@ -96,124 +128,64 @@ class formize {
 		$html .="</select>";	
 		return $html;
 	}
-	
-	public function add_attribute($set,$name,$attr) {
-		$this->field[$set][$name]['attr']=$attr;
-	}
-	
-	public function query($set,$name,$el,$val,$type='text',$label=NULL,$option=array(),$order=NULL) {
+
+	public function fq_submitter($container,$name=NULL,$with_cancel=NULL) {
+		$html="<p class='submit'>";
+		$html .="<input type='submit' value='Submit' name='submit' />";
+		if ($with_cancel) { $html .="<input type='cancel' value='Cancel' name='submit' />"; }
+		$html .="</p>";
+		$this->add($container,$html,$name); 
+	}	
 		
-		$i=$this->index; $html='';
+	public function form_query($container,$el='input',$name,$type='text',$func,$label=NULL,$option=array(),$array_passed=array() ) {
+
+		if (function_exists($func."_func")) { $val =call_user_func($func."_func",$array_passed); } else { $val = $func; }
 		
-		if ($order) { $this->field[$set][$name]['order']=$order; } else { $this->field[$set][$name]['order']=$i; }
-		if ($label) { $this->field[$set][$name]['label']=$label; }
+		$html = "<p>"; if ($label) { $html .="<label>$label</label>"; } 
+		$html .="<span class='value'>";
 		
 		switch($el) {
-			case 'select':
-				$html .=$this->select($name,$val,$option);
-				break;
-			case 'textarea':
-				$html .="<textarea name='$name'>$val</textarea>";
-				break;
-			default:
-				$html .=$this->input($name,$val,$type);
-				break;
+			case 'select':		$html .= $this->fq_select($name,$val,$option); break;
+			case 'textarea':	$html .="<textarea name='$name'>$val</textarea>"; break;
+			default:			$html .=$this->fq_input($name,$val,$type); break;
 		}
-		
-		$this->field[$set][$name]['element']=$html;
-		$this->index++;
+
+		$html .= "</span></p>";
+		$this->add($container,$html,$name); 
+	
 	}
 
-	public function fieldset($w='') {
-		$html="<fieldset name='$w'>\n";
-		$html .="<legend>$w</legend>\n";
-		foreach ( $this->field[$w] as $n) { 
-			$html .="<p>";
-			if ($n['label']) { $html .="<label>". $n['label'] ."</label>"; }
-			if (isset($n['attr'])) { $n['element']=preg_replace ('/(^<\w+.*)(\/>)/',"$1".$n['attr'] ." $2", $n['element'] ); }
-			$html .="<span class='value'>".$n['element']."</span>";
-			$html .="</p>\n";
+	public function fq_addAttribute($container,$name,$attr) {
+		$Q = $this->{$container}['Q']; $n=0; $target=NULL; 
+		foreach ($this->{$container}['Q'] as $Q) {
+			if ($name == $Q['n']) { $target = $Q['h']; $i=$n; } 
+			$n++;
 		}
-		$html .="</fieldset>\n"; 
-		return $html;
+
+		$new_h = preg_replace ('/(^<\w+.*)(name=)/i',"$1".$attr." $2", $target ); 
+		$this->{$container}['Q'][$i]['h'] = $new_h;
 	}
-	
-	public function submitter($with_cancel=NULL) {
-		$html="<p class='submit'>";
-		$html .="<input type='submit' value='submit' name='submit' />";
-		if ($with_cancel) { $html .="<input type='cancel' value='cancel' name='submit' />"; }
-		$html .="</p>\n";
-		return $html;
-	}
-	
-	public function form($set=array(),$id) {
-		$html="<form id='".$id."' action='". $this->action . "' method='". $this->method ."'>\n";
-		foreach ($set as $s) { $html .=$this->fieldset($s); }
-		$html .=$this->submitter();
-		$html .="</form>\n";
-		return $html;
-	}
-	
 }
 
-class stylize {
-
-	public function __construct() { 
-		$this->css=array(); 
-		$this->order=0;
-	}
-
-	public function query($obj,$url,$media,$n=NULL) {
-		if (!$n) { $order=$this->order; } else { $order=$n; }
-		$this->css=array_merge_recursive ( $this->css, array ( $obj=> array( 'order'=> $order, 'name'=> $obj, 'media'=> $media, 'url'=> $url ) ) );
-		$this->order++;
-	}
-
-	public function html() {
-		$html='';
-		$css_toprint=$this->css; sort($css_toprint);
-		foreach ($css_toprint as $k=> $v) { $html .="<link rel='stylesheet' type='text/css' media='".$v['media']."' id='css_".htmlize_sfst($v['name'])."' href='".$v['url']."' />\n"; }
-		if (file_exists(ROOT_PATH."style.css")) { $html .="<link rel='stylesheet' type='text/css' id='css_dev' href='".ROOT_URL."style.css?".time()."' />\n"; }
-		return $html;
-	}
-	
-}	
-
-class scriptize {
-
-	public function __construct() { 
-		$this->js=array(); 
-		$this->order=0;
-	}
-
-	public function query($obj,$url,$n=NULL, $after=false) {
-		if (!$n) { $order=$this->order; } else { $order=$n; }
-		$this->js=array_merge_recursive ( $this->js, array ( $obj=> array( 'order'=> $order, 'name'=> $obj, 'url'=> $url, 'after'=> $after ) ) );
-		$this->order++;
-	}
-
-	public function html($after=false,$temp='') {
-		$html='';
-		$js_toprint=$this->js; sort($js_toprint);
-		if ($temp!='') { $t="?".time(); } else { $t=''; }
-		foreach ($js_toprint as $k=> $v) {
-			if ($after==$v['after']) {
-				$html .="<script type='text/javascript' id='js_".htmlize_sfst($v['name'])."' src='".$v['url'].$t."' /></script>\n"; 
-			}
-		}
-
-		if (($after==true) && (file_exists(ROOT_PATH."function.js"))) { 
-			$html .="<script type='text/javascript' id='js_dev' href='".ROOT_URL."function.js".$t."' /></script>\n"; 
-		}
-	
-		return $html;
-	}
-	
-}
-
-/* utility -------------------------------------------- */
+/* -- Utility ------------------------------------------------------------------------------------------------- */
 
 function htmlize_sfst($s){ return preg_replace('/\W|css|js/','',$s); }
-function htmlize_compress($s){ $s=preg_replace('/\n|\r|\t/i','',$s); $s=preg_replace('/\s+/',' ',$s); return $s; }
+function htmlize_epra($s){ echo "<pre><xmp>"; print_r($s); echo "</xmp></pre>"; }
+function htmlize_delCache($fn) { $fn=HTMLIZE_CACHE_PATH.$fn.".html"; if (file_exists($fn)) { unlink($fn); } }
+function htmlize_getCache($fn) { return file_get_contents(HTMLIZE_CACHE_PATH.$fn.".html"); }
+function htmlize_putCache($fn,$s) {
+	//$s="<!--".time()."--->". PHP_EOL . $s; 
+	file_put_contents(HTMLIZE_CACHE_PATH.$fn.".html" ,$s); 
+	return 'write'; 
+}
+
+function htmlize_valCache($fn,$expire=0,$mode=0) {
+		$fn=HTMLIZE_CACHE_PATH.$fn.".html"; 
+		if (file_exists($fn)) { 
+			if ($expire > (time() - filemtime($fn))) { return 1; } else { return 0; }
+		} else { 
+			if ($mode==true) { return 2; } else { return 0; }
+		}
+}
 
 ?>
